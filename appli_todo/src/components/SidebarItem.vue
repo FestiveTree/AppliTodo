ed0d2b2fb3c37ce6286636a2ff238f91f2ba3e9e
Garout <73251978+Garout@users.noreply.github.com>
2021-03-21 19:51:42 +0100

    Revert "Merge branch 'main' of https://github.com/FestiveTree/AppliTodo into main"
    This reverts commit a3359341a6e69dafeea7733639b3591aed76ffdd, reversing
    changes made to d17eb26890a9c8c20565cc0b5cdf1b733690f2ee.

--- appli_todo/src/components/SidebarItem.vue
+++ appli_todo/src/components/SidebarItem.vue
@@ -2,15 +2,13 @@
 <!-- div for debug -->
 <div style="background-color: green">
   
-  <p>{{ name }} ({{ getTodos(id, "completed").length }}/{{ getTodos(id).length}})</p>
+  <p>{{ name }}</p>
   
 </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { mapGetters } from 'vuex'
-
 
 export default defineComponent({
   name: 'SidebarItem',
@@ -23,10 +21,7 @@ export default defineComponent({
       type: String,
       default: ''
     }
-  },
-  computed: {
-    ...mapGetters('todolist', ['getTodos'])
-  },
+  }
 })
 
 </script>
