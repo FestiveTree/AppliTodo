44997a587b21b3aa2473402f0bbaa4da49f2ee37
21707209 <21707209@etu.unicaen.fr>
2021-03-22 13:30:07 +0100

    ajout du register

--- appli_todo/src/views/Register.vue
+++ appli_todo/src/views/Register.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="my-6 text-center">
-    <h2 class="text-2xl my-2">Page de connexion</h2>
+    <h2 class="text-2xl my-2">Page d'inscription</h2>
     <div class="flex flex-col items-center rounded-full  border-b-2 border-t-2">
       <div class="z-10">
         <p>Identifiant:</p>
         <input class="bg-gray-300 focus:bg-white" v-model="inputUsername" id="user"/>
+        <p>Email:</p>
+        <input class="bg-gray-300 focus:bg-white" v-model="inputEmail" id="mail"/>
         <p>Mot de Passe:</p>
         <input class="bg-gray-300 focus:bg-white" v-model="inputPassword" id="pass" type="password"/>
       </div>
-      <button class=" my-2 bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:border-gray-500 rounded" v-on:click="doLogin">Se connecter</button>
-      <p class="pb-2"> Vous n'avez pas de compte ? <a href="" class="hover:text-blue-500 hover:underline cursor-pointer"> Cliquez ici pour créer un compte.</a> </p>
+
+      <button class=" my-2 bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:border-gray-500 rounded" v-on:click="doRegister">Créer un compte</button>
     </div>
   </div>
 
-  <h1 v-if="displayLoggedOn">Vous êtes connecté!</h1>
+  <h1 v-if="displayLoggedOn">Vous êtes inscrit!</h1>
 </template>
 
 <!-- flex flex-col h-screen my-auto items-center border-8 border-double border-gray-700"-->
@@ -27,26 +29,28 @@ export default defineComponent({
   name: 'Home',
   data() {
           return {
-              inputUsername: 'TestAPI33t6ws@monmail.fr.nf',
-              inputPassword: 'Toto1234##',
+              inputUsername: '',
+              inputPassword: '',
+              inputEmail: '',
               displayLoggedOn: false,
           }
   },
 
   methods: {
-    ...mapActions('account', ['login']),
+    ...mapActions('account', ['register']),
     ...mapActions('todolist', ['load']),
-    doLogin() {
+    doRegister() {
         console.log(this.inputUsername,this.inputPassword)
-        this.login(
+        this.register(
             {
-                email: this.inputUsername,
+                email: this.inputEmail,
                 password: this.inputPassword,
+                username: this.inputUsername,
 
                 //Debut de callback
                 callback: (result) => {
                     if (result.status == 200) {
-                        console.log('Connection effectuee');
+                        console.log('Inscription effectuee');
 
                         //Chargement des todos
                         this.load();
@@ -56,7 +60,7 @@ export default defineComponent({
                         // Redirection.
                         setTimeout(
                             function() {
-                                router.back();
+                                router.push("/");
                             },
                             1000
                         );
