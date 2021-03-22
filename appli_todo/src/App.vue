7813ae3d2d56bab7b1e694fb0f7ac868fc6a4ce7
FestiveTree <58300056+FestiveTree@users.noreply.github.com>
2021-03-22 15:42:15 +0100

    Ajout Deconnexion et Affichage aproprie dans la top bar

--- appli_todo/src/App.vue
+++ appli_todo/src/App.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="flex flex-col items-center">
-  <router-link  to="/" ><img class=" my-1 bg-gray-400 hover:bg-gray-300 rounded m-auto " src="./assets/home.png" width="32" height="32" ></router-link>
-  <router-link class="bg-gray-400 my-2  hover:bg-gray-300 rounded " to="/login">Login -></router-link>
+  <div class="flex items-stretch justify-center w-screen space-x-4">
+    <router-link  to="/" ><img class="p-1 my-2 bg-gray-400 hover:bg-gray-300 rounded " src="./assets/home.png" width="32" height="32" ></router-link>
+    <router-link v-if="!loggedOn" class="p-1 bg-gray-400 my-2  hover:bg-gray-300 rounded " to="/login">Login -></router-link>
+    <router-link v-if="!loggedOn" class="p-1 bg-gray-400 my-2  hover:bg-gray-300 rounded " to="/register">Register -></router-link>
+    <a class="p-1 my-2 bg-gray-400 hover:bg-gray-300 rounded " v-on:click="disconnect">Disconnect</a>
   </div>
   <!-- component matched by the route will render here -->
   <router-view></router-view>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+import { defineComponent } from "@vue/runtime-core"
+import { mapGetters, mapActions } from 'vuex'
+
+export default defineComponent({
+  name: 'App',
+
+  methods: {
+    ...mapActions('account', ['disconnect']),
+  },
+
+  computed: {
+    ...mapGetters('account', ['getterToken']),
+
+    loggedOn() {
+      if (this.getterToken !== null) {
+        return this.getterToken !== null;
+      }
+      return false;
+    }
+  }
+});
 </script>
 
 <!--style>
